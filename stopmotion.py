f4088fc596cd06fcb13f844339dcb32df2b0eb36
Louie Earle <LouieVirosa@users.noreply.github.com>
2020-11-26 16:36:21 -0500

    Rewritten my pythonically after David Beazley course

--- stopmotion.py
+++ stopmotion.py
@@ -1,73 +1,77 @@
 #!/usr/bin/env python3
+'''
+Creates an m4v movie from multiple .jpg images. 
+'''
 
 import argparse
 import datetime
 import os
 import sys
 
-from moviepy.editor import *
+
+MIN_FPS     = 1
+DEFAULT_FPS = 12
+MAX_FPS     = 72
 
 class ctx:
     """
     Empty class we use for saving context
     """
-    pass
+    def __init__(self, fps=DEFAULT_FPS, src=None, dst=None, name=None):
+        self.fps = fps
+        if src is None:
+            src = os.getcwd()
+        self.src = os.path.abspath(src)
+        if dst is None:
+            dst = os.getcwd()
+        self.dst = os.path.abspath(dst)
+        if name is None:
+            name = datetime.datetime.now().strftime('%Y_%m_%d_%H-%M-%S.m4v')
+        self.name = name
 
-def init_argparser(ctx):
+        assert MIN_FPS <= fps <= MAX_FPS, f"FPS must be between {MIN_FPS} and {MAX_FPS} (inclusive)" 
+        assert os.path.isdir(self.src), f"Source directory {self.dst} does not exist."
+        assert os.path.isdir(self.dst), f"Destination directory {self.dst} does not exist."
+        self._get_files()
+
+    def _get_files(self):
+        self.files = sorted([ os.path.join(self.src, f) for f in os.listdir(self.src) if f.lower().endswith(".jpg") ])
+        
+    def __len__(self):
+        return len(self.files)
+
+    def __getattr__(self, name):
+        if name == 'frame_duration':
+            return 1 / self.fps
+        elif name == 'movie_len':
+            return self.__len__() / self.fps
+        elif name == 'full_name':
+            return os.path.join(self.dst, self.name)
+        raise AttributeError(f"\'{self}\' has no attribute \'{name}\'")
+    
+    def generate_clip(self):
+        # Import included here because it takes a while to load...
+        from moviepy.editor import concatenate_videoclips, ImageClip
+        clips = [ ImageClip(f).set_duration(self.frame_duration) for f in self.files ]
+        final_clip = concatenate_videoclips(clips)
+        final_clip.write_videofile(self.full_name, fps=self.fps, codec='libx264')
+
+def init():
     """
     Make sure we get the destination directory, the filename we want to save as, and
     how many frames per second we want. 
     """
-    default_name = datetime.datetime.now().strftime('%Y_%m_%d_%H-%M-%S.m4v')
     parser = argparse.ArgumentParser()
     parser.add_argument("--fps", help="Number of images to create one second of video", type=int, default=12)
-    parser.add_argument("--dst", help="Destination folder for completed movie", default=os.getcwd())
-    parser.add_argument("--src", help="Source folder for images", default=os.getcwd())
-    parser.add_argument("--out", help="Output Name", dest="output", default=default_name)
+    parser.add_argument("--dst", help="Destination folder for completed movie")
+    parser.add_argument("--src", help="Source folder for images")
+    parser.add_argument("--out", help="Output Name", dest="output")
     res = parser.parse_args()
-
-    if not os.path.isdir(res.src):
-        parser.print_help(sys.stderr)
-        print("\n--src directory specified does not exist.\n")
-        sys.exit(1)
-    
-    if not os.path.isdir(res.dst):
-        parser.print_help(sys.stderr)
-        print("\n--dst directory specified does not exist.\n")
-        sys.exit(1)
-   
-    if res.fps > 72 or res.fps < 1:
-        parser.print_help(sys.stderr)
-        print("\n--fps must be an integer within 1 and 72, inclusive.\n")
-        sys.exit(1)
-        
-         
-    ctx.fps = res.fps
-    ctx.dst = res.dst
-    ctx.src = res.src
-    ctx.name = res.output
+    return ctx(fps=res.fps, src=res.src, dst=res.dst, name=res.output)
 
 
 if __name__ == "__main__":
-    ctx = ctx()
-    init_argparser(ctx)
-    
-    name = os.path.join(ctx.dst, ctx.name)
-    duration = 1 / ctx.fps
-
-    files = sorted([os.path.join(ctx.src,f) for f in os.listdir(ctx.src) if f.endswith(".jpg") or f.endswith(".JPG")])
-    if len(files) < 1:
-        print("No images ending in .jpg found in src directory.")
-        sys.exit(1)
-
-    clips = [ImageClip(f).set_duration(duration) for f in files]    
-
-    final_clip = concatenate_videoclips(clips)
-    
-    num_imgs = len(files)
-    print("Found %d images." % num_imgs)
-    print("Creating a %f second long video: %s" % (num_imgs/ctx.fps, name))
-
-    final_clip.write_videofile(name, fps=ctx.fps, codec='libx264')
-             
+    ctx = init()
+    print(f"Creating a {ctx.movie_len} second long video from {len(ctx)} images: {ctx.full_name}")
+    ctx.generate_clip()
     sys.exit(0)
