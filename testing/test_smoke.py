c811b39e832ac7f51919bc6641766a1a5301a143
Louie Earle <LouieVirosa@users.noreply.github.com>
2020-11-27 19:58:46 -0500

    Added negative cases to smoke test suite

--- testing/test_smoke.py
+++ testing/test_smoke.py
@@ -2,11 +2,13 @@
 All tests here are for verification of basic functionality of this project.
 '''
 import os
+import random
 
 from moviepy.editor import VideoFileClip
+import pytest
 
 from conftest import TLD, yellow_print, green_print
-from stopmotion import StopMotionCTX #pylint: disable=import-error
+from stopmotion import StopMotionCTX, MIN_FPS, MAX_FPS #pylint: disable=import-error
 
 LENGTH_MIN_ACCURACY = 0.1
 
@@ -19,8 +21,9 @@ def test_builds_correctly():
     dest_dir = "/tmp/"
     filename = "test_KROC.mp4"
 
-    ctx = StopMotionCTX(src=src_dir, dst=dest_dir, name=filename)
-    yellow_print(f"Creating a {ctx.movie_len:.2f} second movie at "
+    fps = random.randrange(MIN_FPS, MAX_FPS + 1)
+    ctx = StopMotionCTX(src=src_dir, dst=dest_dir, fps=fps, name=filename)
+    yellow_print(f"Creating a {ctx.movie_len:.2f} second movie at {ctx.fps} fps: "
         f"{os.path.join(dest_dir, filename)}...")
     ctx.generate_clip()
     green_print("Created.")
@@ -35,3 +38,32 @@ def test_builds_correctly():
     yellow_print("Removing created video...")
     assert os.remove(ctx.full_name) is None
     green_print("Video file removed.")
+
+
+def test_throw_errors():
+    '''
+    This test ensures that exceptions are raised if we try to create a stopmotion context
+    with bad params
+    '''
+    non_existing_dir = "/DOESNOTEXIST/"
+    assert not os.path.isdir(non_existing_dir)
+
+    yellow_print("Attempting to create StopMotionCTX with invalid source dir...")
+    with pytest.raises(AssertionError):
+        StopMotionCTX(src=non_existing_dir)
+    green_print("Threw exception (expected)")
+
+    yellow_print("Attempting to create StopMotionCTX with invalid dest dir...")
+    with pytest.raises(AssertionError):
+        StopMotionCTX(dst=non_existing_dir)
+    green_print("Threw exception (expected)")
+
+    yellow_print("Attempting to create StopMotionCTX with FPS too low...")
+    with pytest.raises(AssertionError):
+        StopMotionCTX(fps=MIN_FPS - 1)
+    green_print("Threw exception (expected)")
+
+    yellow_print("Attempting to create StopMotionCTX with FPS too high...")
+    with pytest.raises(AssertionError):
+        StopMotionCTX(fps=MAX_FPS + 1)
+    green_print("Threw exception (expected)")
